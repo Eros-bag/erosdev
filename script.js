157f10d551133b9e17e1026f562d1639ac1a4cf0
Eros-bag <balogoungeoffroy274@gmail.com>
2025-11-01 03:53:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,12 +36,3 @@ window.addEventListener('load', () => {
   });
 });
 
-const burger = document.querySelector(".burger");
-const nav = document.querySelector(".nav-links");
-
-burger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  // Animation des lignes
-  burger.classList.toggle("toggle");
-});
-
